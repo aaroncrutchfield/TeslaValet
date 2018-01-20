a23b0093f85317a35f131154bed0533a9412274a
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2018-01-20 16:27:30 -0500

    feat: Add LocationServices and Places API

--- app/src/main/java/com/example/ioutd/teslavalet/MainActivity.java
+++ app/src/main/java/com/example/ioutd/teslavalet/MainActivity.java
@@ -1,25 +1,27 @@
 package com.example.ioutd.teslavalet;
 
-import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothHeadset;
-import android.bluetooth.BluetoothProfile;
-import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.util.List;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Places;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final int REQUEST_ENABLE_BT = 1;
-
-    // Get the default bluetooth adapter
-    BluetoothHeadset bluetoothHeadset;
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
 
     @Override
@@ -33,20 +35,52 @@ public class MainActivity extends AppCompatActivity {
         BluetoothBroadcastReceiver receiver = new BluetoothBroadcastReceiver();
         IntentFilter filter = new IntentFilter();
 
-        filter.
         filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
         filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
 
         registerReceiver(receiver, filter);
 
-        new Runnable() {
-            @Override
-            public void run() {
 
-            }
-        };
+        requestLocationPermissions();
+        buildLocationApiClient();
     }
 
+    private void requestLocationPermissions() {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+    }
 
+    private void buildLocationApiClient() {
+        GoogleApiClient client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(Places.GEO_DATA_API)
+                .enableAutoManage(this, this)
+                .build();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "API Client Connection Successful!");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "API Client Connection Failed!");
+    }
 }
