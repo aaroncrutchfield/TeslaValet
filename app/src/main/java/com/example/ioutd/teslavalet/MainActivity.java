2ef39a19440cb2af48cb0cbbf664814c5ee1c4a4
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2018-01-20 19:14:33 -0500

    feat: Get the current gps location

--- app/src/main/java/com/example/ioutd/teslavalet/MainActivity.java
+++ app/src/main/java/com/example/ioutd/teslavalet/MainActivity.java
@@ -3,25 +3,38 @@ package com.example.ioutd.teslavalet;
 import android.bluetooth.BluetoothDevice;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.ActionBar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class MainActivity extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
+
+    private GeoDataClient geoDataClient;
+    private PlaceDetectionClient placesDetectionClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private boolean mLocationPermissionGranted;
+    private LatLng coordinates;
 
 
     @Override
@@ -29,9 +42,18 @@ public class MainActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("this is mine");
+        // Construct a GeoDataClient
+        geoDataClient = Places.getGeoDataClient(this, null);
+        placesDetectionClient = Places.getPlaceDetectionClient(this, null);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
+        setupBroadcastReceiver();
+
+        requestLocationPermissions();
+        getCurrentLocation();
+    }
+
+    private void setupBroadcastReceiver() {
         BluetoothBroadcastReceiver receiver = new BluetoothBroadcastReceiver();
         IntentFilter filter = new IntentFilter();
 
@@ -39,30 +61,56 @@ public class MainActivity extends AppCompatActivity implements
         filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
 
         registerReceiver(receiver, filter);
-
-
-        requestLocationPermissions();
-        buildLocationApiClient();
     }
 
     private void requestLocationPermissions() {
-        if (ActivityCompat.checkSelfPermission(MainActivity.this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MainActivity.this,
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
                     new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                     PERMISSIONS_REQUEST_FINE_LOCATION);
         }
     }
 
-    private void buildLocationApiClient() {
-        GoogleApiClient client = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .addApi(Places.GEO_DATA_API)
-                .enableAutoManage(this, this)
-                .build();
+    private void getCurrentLocation() {
+        if (mLocationPermissionGranted) {
+            try {
+                Task locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            Location lastKnownLocation = (Location) task.getResult();
+
+                            coordinates = new LatLng(lastKnownLocation.getLatitude(),
+                                    lastKnownLocation.getLongitude());
+                            Log.d(TAG, "getCurrentLocation: " + coordinates.toString());
+                        }
+                    }
+
+                });
+            } catch (SecurityException e){
+                throw new UnsupportedOperationException("Location permission have not been granted");
+            }
+        }
     }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: was called");
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_FINE_LOCATION:
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    mLocationPermissionGranted = true;
+        }
+    }
+
 
     @Override
     protected void onResume() {
